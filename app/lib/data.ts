f0725635e59b3a920000380d8c58ce682faa8b6c
Narendra kumar <narendrakr9870@gmail.com>
2025-04-07 15:44:30 +0530

    Authentication and meta

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -156,10 +156,9 @@ export async function fetchInvoiceById(id: string) {
 
     const invoice = data.map((invoice) => ({
       ...invoice,
-      // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
